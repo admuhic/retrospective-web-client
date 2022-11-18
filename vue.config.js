151cbb58f94de7c81399c2f96bec173e1114ea29
Admir <admir.muhic@yahoo.com>
2022-11-18 15:29:43 +0100

    Deploy to GitHub pages configuration made

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/retrospective-web-client/" : "/",
+  publicPath: "/retrospective-web-client/",
   transpileDependencies: true,
   css: {
     loaderOptions: {
